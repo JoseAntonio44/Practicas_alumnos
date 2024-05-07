405f30edd45189f1c2b0f7e8bb841ac936219e7d
JoseAntonio44 <josantgar44@gmail.com>
2024-05-07 15:55:40 +0200

    CRUD hecho

--- alumnos.php
+++ alumnos.php
@@ -1,6 +1,8 @@
 <?php
 $nombre=$_POST['nombre'] ?? null;
+$nombre_B=$_POST['nombre_B'] ?? null;
 $num_paginas = intval($_POST['pagina'] ?? 1);
+$alumno = array(); //array para guardar los datos del alumno
 
 ?>
 <?php
@@ -38,12 +40,12 @@ $num_paginas = intval($_POST['pagina'] ?? 1);
     <title>Alumnos</title>
 </head>
 <body>
- <form action="alumnos.php" method="post">
+<form action="alumnos.php" method="post">
   	<label for="nombre">NOMBRE: </label>
-  	<input type="text" name="nombre" id="nombre" value="<?php echo $nombre?>">
+  	<input type="text" name="nombre_B" id="nombre" value="<?php echo $nombre_B?>">
   	<input type="submit" value="Buscar">
   	<input type="reset" value="Reset">
-  </form>
+</form>
 
 <?php
 
@@ -61,16 +63,16 @@ $num_paginas = intval($_POST['pagina'] ?? 1);
     $registros = ($num_paginas-1)*$registros_pagina;
 
     
-    $sql = "SELECT nia, nombre,telefono,cv_file ,email FROM alumno where true limit $registros, $registros_pagina";
-    if(isset($_POST["nombre"])){
-      $sql = "SELECT nia, nombre,telefono,cv_file ,email FROM alumno where true and nombre like :nombre limit $registros, $registros_pagina"; 
+    $sql = "SELECT nia, nombre, cv_file, telefono,email FROM alumno where true limit $registros, $registros_pagina";
+    if(isset($_POST["nombre_B"])){
+      $sql = "SELECT nia, nombre, cv_file,telefono ,email FROM alumno where true and nombre like :nombre limit $registros, $registros_pagina"; 
     }
     $gsent = $pdo->prepare($sql);
-    if(isset($_POST["nombre"])){
+    if(isset($_POST["nombre_B"])){
         
-      $nombre = '%' . $_POST["nombre"] . '%';
+      $nombre_B = '%' . $_POST["nombre_B"] . '%';
 
-      $gsent->bindParam(':nombre', $nombre, PDO::PARAM_STR);
+      $gsent->bindParam(':nombre', $nombre_B, PDO::PARAM_STR);
     }
     $gsent->execute();
 
@@ -78,13 +80,41 @@ $num_paginas = intval($_POST['pagina'] ?? 1);
       echo "<table>";
     	echo "<tr><th>NIA</th><th>Nombre</th><th>Telefono</th><th>Email</th><th>CV</th></tr>";
     	while ($row = $gsent->fetch(PDO::FETCH_ASSOC)) {
-      echo "<tr><td>".$row['nia']."</td><td>".$row['nombre']."</td><td>".$row['telefono']."</td><td>".$row['email']."</td><td>".$row['cv_file']."</td></tr>";	
+      echo "<tr><td>".$row['nia']."</td><td>".$row['nombre']."</td><td>".$row['telefono']."</td><td>".$row['email']."</td><td>".$row['cv_file']."</td>";
+
+      // Boton "Editar"
+      echo "<td>";
+      echo "<form method='post' action='alumnos.php'>";
+      echo "<input type='hidden' name='nia' value='" . $row['nia'] . "'>";
+      echo "<input type='submit' value='Editar'>";
+      echo "</form>";
+
+      // Boton "Eliminar"
+      echo "<td><form method='post' action='alumnos.php'>
+            <input type='hidden' name='nia_d' value='" . $row['nia'] . "'>
+            <input type='submit' name='eliminar_alumno' value='Eliminar'></form></td>";
+
+      echo "</tr>";
     }
     echo "</table>";
+
+
+    //Recogida de datos para la edicion de los alumnos
+    if(isset($_POST['nia'])){
+      $nia = $_POST['nia'];
+      $sql = "SELECT * FROM alumno WHERE nia = :nia"; //aqui se recoge los datos del alumno seleccionado
+      $gsent = $pdo->prepare($sql);
+      $gsent->bindParam(':nia', $nia, PDO::PARAM_INT);
+      $gsent->execute();
+      $alumno = $gsent->fetch(PDO::FETCH_ASSOC);
+    }
+    ?>
+    
+    <?php
 	  }catch(PDOException $err) {
     echo "Error al ejecutar la consulta"; 
 	  }
-  ?>
+    ?>
   <?php
   if(isset($_POST["siguiente_pagina"])){
       ++$num_paginas;
@@ -100,7 +130,10 @@ $num_paginas = intval($_POST['pagina'] ?? 1);
   if(isset($_POST["primera_pagina"])){ 
     $num_paginas = 1;
   }
+  
   ?>
+
+   <!--Paginación-->
   <form action="alumnos.php" method="post">
     <input type="submit" name="primera_pagina" value="<<" <?php ?>>
     <input type="submit" name="pagina_anterior" value="<" <?php ?>>
@@ -109,11 +142,48 @@ $num_paginas = intval($_POST['pagina'] ?? 1);
     <input type="submit" name="ultima_pagina" value=">>" <?php ?>>
   </form>
 
+  <?php
+  if(isset($alumno['nia']) || isset($alumno['nombre']) || isset($alumno['telefono']) || isset($alumno['email'])) {
+  ?>
+
+  <!--Formulario editar datos alumno-->
+  <h2>Editar alumno</h2>
+    <form method="post" action="alumnos.php">
+            <input type="hidden" name="nia" value="<?php echo $alumno['nia']; ?>">
+            Nombre: <input type="text" name="nombre" value="<?php echo $alumno['nombre']; ?>"><br>
+            Telefono: <input type="text" name="telefono" value="<?php echo $alumno['telefono']; ?>"><br>
+            Email: <input type="text" name="email" value="<?php echo $alumno['email']; ?>"><br>
+            
+            <input type="submit" value="Actualizar">
+    </form>
+
   
+  <?php
+  $nia_M = $_POST['nia'];
+  $nombre_M = $_POST['nombre']??null;
+  $telefono_M = $_POST['telefono']??null;
+  $email_M = $_POST['email']??null;
+
+  $sql = "UPDATE alumno SET nombre = :nombre_M, telefono = :telefono_M, email = :email_M WHERE nia = :nia_M";
+  $stmt = $pdo->prepare($sql);
+
+  
+  //ejecuta la consulta para actualizar los datos
+  $stmt->execute([
+    ':nombre_M' => $nombre_M,
+    ':telefono_M' => $telefono_M,
+    ':email_M' => $email_M,
+    ':nia_M' => $nia_M
+  ]);
+
+
+  }
+  ?>
+
   <h2>Insertar alumno nuevo:</h2>
   <form action="alumnos.php" method="post">
-  <label for="nia">NIA*: </label>
-  <input type="text" name="nia" id="nia" pattern="[0-9]{8}">
+  <label for="nia_I">NIA*: </label>
+  <input type="text" name="nia_I" id="nia_I" pattern="[0-9]{8}">
   <label for="nombre_i">Nombre*: </label>
   <input type="text" name="nombre_i" id="nombre_i">
   <label for="telefono">Telefono: </label>
@@ -123,16 +193,17 @@ $num_paginas = intval($_POST['pagina'] ?? 1);
 
   <input type="submit" value="Insertar">
   <input type="reset" value="Reset">
+
 </form>
   <?php
   try{
-    if(isset($_POST['nia']) && !empty($_POST['nia'])) {
+    if(isset($_POST['nia_I']) && !empty($_POST['nia_I'])) {
       
-      $nia=$_POST['nia'];
+      $nia_I=$_POST['nia_I'];
 
       //Se comprueba que el nia no está repetido
-      $comprobarNia = $pdo->prepare("SELECT COUNT(*) FROM alumno WHERE nia = :nia");
-      $comprobarNia->bindParam(':nia', $nia);
+      $comprobarNia = $pdo->prepare("SELECT COUNT(*) FROM alumno WHERE nia = :nia_I");
+      $comprobarNia->bindParam(':nia_I', $nia_I);
       $comprobarNia->execute();
       $comprobar = $comprobarNia->fetchColumn(); //se guarda en una variable las filas que coinciden con el nia introducido
 
@@ -142,8 +213,8 @@ $num_paginas = intval($_POST['pagina'] ?? 1);
         $telefono=$_POST['telefono'] ?? null;
         $email = $_POST['email'] ?? null;
 
-        $sql = "insert into alumno (nia, nombre, telefono, email) values (:nia, :nombre, :telefono, :email)";
-        $datos = [":nia"=>$nia, ":nombre"=>$nombre_i, ":telefono"=>$telefono, ":email"=>$email];
+        $sql = "insert into alumno (nia, nombre, telefono, email) values (:nia_I, :nombre, :telefono, :email)";
+        $datos = [":nia_I"=>$nia_I, ":nombre"=>$nombre_i, ":telefono"=>$telefono, ":email"=>$email];
         $stmt = $pdo->prepare($sql);
         $row = $stmt->execute($datos);
 
@@ -156,25 +227,19 @@ $num_paginas = intval($_POST['pagina'] ?? 1);
   echo $e->getMessage();
   }
   ?>
-  
-  <h2>Eliminar alumno:</h2>
-  <form action="alumnos.php" method="post">
-  <label for="nia">NIA*: </label>
-  <input type="text" name="nia_d" id="nia_d" pattern="[0-9]{8}">
-
-  <input type="submit" value="Eliminar">
-  <input type="reset" value="Reset">
 
   <?php
   try{
-    $nia_d = $_POST['nia_d'] ?? null;
-  if(isset($_POST["nia_d"])){
     
+  if(isset($_POST["eliminar_alumno"])){
+    
+    $nia_d = $_POST['nia_d'] ?? null;
 
     $sql = "DELETE FROM alumno where nia = :nia"; 
     $stmt = $pdo->prepare($sql);
     $stmt->bindParam(':nia',$nia_d);
     $stmt->execute();
+
 
     echo "Alumno con NIA $nia_d eliminado correctamente.";
   }
